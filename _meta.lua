4caffea9839c5121be27de5be3202f8731bd6693
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 15:47:53 +0200

    chore: bump version to 1.1.4

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "mastermind",
     fullname    = _("Mastermind"),
     description = _("Deduce the secret code from black and white peg feedback."),
-    version     = "1.1.3",
+    version     = "1.1.4",
 }
